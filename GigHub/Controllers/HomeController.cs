7f80a7da17c27d86cfb8b939782fd7a694b3431a
mohammad-bilal <mohammad.bilal.ga@gmail.com>
2017-12-21 12:10:51 +0530

    Added name to Artsit

--- GigHub/Controllers/HomeController.cs
+++ GigHub/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GigHub.Models;
+using System.Data.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +10,15 @@ namespace GigHub.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            var upcomingGigs = _context.Gigs.Include(g => g.Artist).Where(g => g.DateTime > DateTime.Now).ToList();
+            return View(upcomingGigs);
         }
     }
 }
